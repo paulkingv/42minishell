fea3881566a28aab359c936d510c8d91f84ceaa3
Paul King <pking@1H4.42angouleme.fr>
2026-06-13 17:43:41 +0200

    added header and main

---
+++ include/minishell.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pking <pking@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/13 16:31:46 by pking             #+#    #+#             */
+/*   Updated: 2026/06/13 16:31:48 by pking            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+#define MINISHELL_H
+
+#include <stdio.h> //debugging
+#include <unistd.h> // pipes, fork, getpid, execve, dup2
+#include <sys/wait.h> // waitpid(),
+#include <sys/types.h> // pid_t datatype,
+
+/*------STRUCTS------*/
+
+
+/*-----FUNCTIONS-----*/
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pking <pking@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/13 16:31:39 by pking             #+#    #+#             */
+/*   Updated: 2026/06/13 17:42:40 by pking            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int main (void)
+{
+	// Kick off the Shell
+
+	// Wait for arguments to come in, in a loop
+
+	// Take arguments -> Parser
+
+	// Return Parser arguments -> Executor
+
+	// Return Executor result -> Shell
+
+	return (0);
+}
