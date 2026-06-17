c87e3e3eced6c9336ac2e693140bb5eb9192c0a7
Paul King <pvx0x@Pauls-MacBook-Pro.local>
2026-06-17 15:07:28 +0200

    added more includes to minishell.h

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: pking <pking@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/13 16:31:46 by pking             #+#    #+#             */
-/*   Updated: 2026/06/13 16:31:48 by pking            ###   ########.fr       */
+/*   Updated: 2026/06/17 14:48:42 by pking            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,9 @@
 #include <unistd.h> // pipes, fork, getpid, execve, dup2
 #include <sys/wait.h> // waitpid(),
 #include <sys/types.h> // pid_t datatype,
+#include <readline/readline.h> // for readline
+#include <readline/history.h> //for readline's history (sh history)
+#include <stdlib.h> //malloc
 
 /*------STRUCTS------*/
 
