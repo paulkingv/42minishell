5a4264aba74f3824b54e2559dba186c6a9abe0f1
johnmatthewfoxpro <johnmatthewfoxpro@gmail.com>
2026-06-17 15:33:57 +0200

    Cleaned up repo - ready to advance

---
+++ Planning Folder/Oceano_Demo/main.c
@@ -0,0 +1,13 @@
+#include "shell.h"
+
+int main (int argc, char **argv)
+{
+    //REPL
+    // Read -> Eval -> Print/Exec -> Loop
+    while()
+    {
+        // 1) get line
+
+        // get token
+    }
+}
