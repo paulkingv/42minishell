2f62adfdad1d326564af598a84210a463383429c
Paul King <pvx0x@Pauls-MacBook-Pro.local>
2026-06-17 15:07:58 +0200

    added includes in minishell.h

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: pking <pking@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/13 16:31:39 by pking             #+#    #+#             */
-/*   Updated: 2026/06/13 17:42:40 by pking            ###   ########.fr       */
+/*   Updated: 2026/06/17 15:07:22 by pking            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int main (void)
 {
 	// Kick off the Shell
-
+	
 	// Wait for arguments to come in, in a loop
 
 	// Take arguments -> Parser
