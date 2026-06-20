c4d37133e4073d5b30c82733e4fea7180a1803c7
Paul King <pvx0x@Pauls-MacBook-Pro.local>
2026-06-20 15:11:31 +0200

    added the token structs

--- include/minishell.h
+++ include/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jfox <jfox.42angouleme@gmail.com>          +#+  +:+       +#+        */
+/*   By: pking <pking@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/13 16:31:46 by pking             #+#    #+#             */
-/*   Updated: 2026/06/17 16:42:40 by jfox             ###   ########.fr       */
+/*   Updated: 2026/06/20 15:10:36 by pking            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,27 @@
 
 /*------STRUCTS------*/
 
-//hELLO
+// TOKENS
+// 2 Structs: Token and Token Type (Token Type first because Token needs it)
+
+typedef enum e_token_type
+{
+    //CHECK WITH OTHERS IF ENUMS ARE ALLOWED
+    TOKEN_WORD,
+    TOKEN_PIPE,
+    TOKEN_REDIR_OUT,
+    TOKEN_REDIR_IN,
+    TOKEN_APPEND,
+    TOKEN_HEREDOC,
+    TOKEN_EOF
+}   t_token_type;
+
+typedef struct s_token
+{
+    t_token_type    type;
+    char            *value;
+    struct s_token  *next_token;
+}   t_token;
 
 /*-----FUNCTIONS-----*/
 
