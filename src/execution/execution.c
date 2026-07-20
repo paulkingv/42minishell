d12d034511390f6c3ea6ddad60d2899841ce7264
pking <paulbking5@gmail.com>
2026-07-21 01:28:08 +0200

    fixed leaks in execution.c [child_exe_cmd; parent_cleanup_exe_cmd]

--- src/execution/execution.c
+++ src/execution/execution.c
@@ -6,7 +6,7 @@
 /*   By: pking <pking@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/24 18:09:24 by pking             #+#    #+#             */
-/*   Updated: 2026/07/20 14:16:32 by pking            ###   ########.fr       */
+/*   Updated: 2026/07/21 01:27:23 by pking            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,11 @@
 // returns the number of the write end of pipe to the prev_fd for use in next iter
 // Also handles cleaning up (closing)
 // Added tmp struct to not touch shell.
-static int parent_cleanup_exe_cmd(int prev_fd, int pipe_fd[2], t_shell *shell)
+static int parent_cleanup_exe_cmd(int prev_fd, int pipe_fd[2], t_shell *shell, t_cmd *cmdline)
 {
-	t_cmd	*cmd_line = NULL;
-
-	cmd_line = shell->cmdline;
 	if (prev_fd != -1)
 		close(prev_fd);
-	if (cmd_line)
+	if (cmdline->next)
 	{
 		close(pipe_fd[1]);
 		return(pipe_fd[0]);
@@ -47,8 +44,8 @@ static void child_exe_cmd(int prev_fd, int pipe_fd[2], t_shell *shell)
 	}
 	if (cmdline->next) // have to set up where we write to. Pipe FD overwrite STDOUT
 		safe_dup2(pipe_fd[1], STDOUT_FILENO);
-
-	exec_close_pipe(pipe_fd);
+	if (cmdline->next)
+		exec_close_pipe(pipe_fd);
 	if (is_redir)
 		handle_redirects(is_redir);
 	envp = env_to_array(shell->env);
@@ -79,12 +76,14 @@ void exe_cmdline(t_shell *shell)
 		// 	cmdline = cmdline->next;
 		// 	continue; // this keyword resets the while(cmd) loop from the top
 		// }
+		pipe_fd[0] = -1;
+		pipe_fd[1] = -1;
 		if (cmdline->next)
 			safe_pipe(pipe_fd); // error handling pipe improved function
 		pid = safe_fork(); // error handling fork improved function
 		if (pid == 0)
 			child_exe_cmd(prev_fd, pipe_fd, shell);
-		prev_fd = parent_cleanup_exe_cmd(prev_fd, pipe_fd, shell);
+		prev_fd = parent_cleanup_exe_cmd(prev_fd, pipe_fd, shell, cmdline);
 		cmdline = cmdline->next;
 	}
 	while (waitpid(-1, &exit, 0) > 0) // Special waiting line (Need 2 research)
