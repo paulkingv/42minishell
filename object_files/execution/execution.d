d14ee1c4bd5b97293d4ecd508a6126de2bffa5fc
pking <paulbking5@gmail.com>
2026-07-22 03:59:40 +0200

    bug fixes, frees, got rid of unnessesary code

--- object_files/execution/execution.d
+++
@@ -1,6 +0,0 @@
-object_files/execution/execution.o: \
-  /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/SDKSettings.json \
-  src/execution/execution.c include/minishell.h include/libft.h
-/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/SDKSettings.json:
-include/minishell.h:
-include/libft.h:
