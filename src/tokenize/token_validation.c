3bf48feb57f2cdfefb24ea33dd6da2bded136eac
pking <paulbking5@gmail.com>
2026-08-22 23:11:16 +0200

    pushing to save the history

--- src/tokenize/token_validation.c
+++ src/tokenize/token_validation.c
@@ -6,39 +6,37 @@
 /*   By: pking <pking@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/21 02:31:14 by pking             #+#    #+#             */
-/*   Updated: 2026/08/21 04:35:31 by pking            ###   ########.fr       */
+/*   Updated: 2026/08/22 00:29:36 by pking            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static int token_syntax_error(int type, t_token *clone)
+static void token_syntax_error (t_token *token)
 {
-	if (type == 1)
+	if (token->type == PIPE)
 	{
-		perror("bash: syntax error near unexpected token `|'"); //bash doesnt work like this idk 5.3
+		if (token->type == NULL)
+			ft_putstr_fd("syntax error near unexpected token `newline'\n", 2); 
+		else
+
 		type = 127;
 	}
-	else if (type == 2)
+	else
 	{
-		if (clone->next == NULL)
-			perror("syntax error near unexpected token `|'");
-		else if (clone->next->type & PIPE)
-			perror("syntax error near unexpected token `||'");
-		type = 2;
+		if(token->next == NULL)
+			ft_putstr_fd("syntax error near unexpected token `newline'\n");
+		else
+		{
+			ft_putstr_fd("syntax error near unexpected token `")
+			ft_putstr_fd(token->next->value, 2);
+		}
+		if (token->next != NUll)
+			ft_putstr_fd("'\n", 2);
 	}
-	else if (type == 3)
-	{
-		if (clone->next == NULL)
-			perror("syntax error near unexpected token `newline'");
-		else if (clone->next->type != WORD)
-			perror("syntax error near unexpected token"); // adding logic 2 this is too much
-		type = 258;
-	}
-	return (type);
 }
 
-static int validate_tokens(t_token *token, int error) // exit value needs to get changed
+static int validate_tokens(t_token *token) // exit value needs to get changed
 {
 	t_token *clone;
 
@@ -46,19 +44,19 @@ static int validate_tokens(t_token *token, int error) // exit value needs to get
 	if (!clone)
 		return (-1);
 	if ((clone->type & PIPE)) //only happens first time funct runs - check pipe as first
-		error = token_syntax_error (1, clone);
+		error = token_syntax_error (clone);
 	while (clone) // check the rest of tokens
 	{
 	// first, pipe cant be last or followed by pipe
 		if ((clone->type & PIPE)) // >,<,>>
 		{
 			if ((clone->next == NULL) || (clone->next->type & PIPE))
-				error = token_syntax_error(2, clone);
+				error = token_syntax_error(clone);
 		}
 	// if clone == REDIR && (we are last token || next->type isnt word)
 		if (((clone->type & REDIR_MASK))
 		&& ((clone->next == NULL) || (clone->next->type != WORD)))
-			error = token_syntax_error(3, clone);
+			error = token_syntax_error(clone);
 		clone = clone->next;
 	}
 	return (error);
